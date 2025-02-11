b3933214e6db0cc1aa7108d1f6426d048367003a
gee989185 <gee020915@gmail.com>
2025-02-11 15:53:16 +0900

    v2.2 day02 prime number

--- day02.py
+++ day02.py
@@ -1,9 +1,20 @@
-# preview
-subjects = "python c++ database linux"
-print('%e' % 0.7045)
-print(subjects.isalnum())
-subject = input("수강신청과목 입력 : ")
-try:
-    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
-except ValueError:
-    print('해당과목이 존재하지 않습니다')
+# prime number
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+if n1 > n2:     #packing unpacking
+    n1, n2 = n2, n1
+
+for number in range(n1, n2+1):
+    is_prime = True
+
+    if number < 2:
+        #pass
+        continue
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime: print(number, end=' ')
