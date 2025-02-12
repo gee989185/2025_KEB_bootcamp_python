675267ad3f0346465d71ff59d29424726fc63c96
gee989185 <gee020915@gmail.com>
2025-02-12 20:09:01 +0900

    v2.6 day03 exercise

--- day03.py
+++ day03.py
@@ -4,20 +4,11 @@
 # while문 안 쪽의 조건문을 줄이세요 hint. int로 하기
 import random
 
-# 리스트로 변경
+# 술(sul), 안주(anzu) 설정: 리스트로 설정
 drinks_foods_sul = ["위스키", "와인","소주","고량주"]
 drinks_foods_anzu = ["초콜릿","치즈", "삼겹살", "양꼬치" ]
 
-# 리스트로 변경
-# japan_drinks_foods_sul1 = "사케"
-# japan_drinks_foods_sul2 = "위스키"
-# drinks_foods_sul.append(japan_drinks_foods_sul1)
-# drinks_foods_sul.append(japan_drinks_foods_sul2)
-# japan_drinks_foods_anzu1 = "광어회"
-# japan_drinks_foods_anzu2 = "낙곱새"
-# drinks_foods_anzu.append(japan_drinks_foods_anzu1)
-# drinks_foods_anzu.append(japan_drinks_foods_anzu2)
-
+# 술, 안주 추가
 drinks_foods_sul.extend(["사케", "막걸리"])
 drinks_foods_anzu.extend(["광어회", "낙곱새"])
 
@@ -31,7 +22,7 @@ menu_list = '다음  술 중에 고르세요\n'
 for i in range(len(drinks_foods_sul)):
     menu_list = menu_list +f'{i+1}) {drinks_foods_sul[i]}\t\t'
 menu_list = menu_list + f'{len(drinks_foods_sul) +1}) 아무거나\t\t {len(drinks_foods_anzu)+2} 종료 :'
-# print(menu_list)
+
 
 while True:
     menu = int(input(menu_list))
