a45e4ced073714a689ecd7952be388569174eac8
gee989185 <gee020915@gmail.com>
2025-02-12 13:34:37 +0900

    v2.4 day03

--- day03.py
+++ day03.py
@@ -1,40 +1,19 @@
-# Assignment Day 02
-# v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
-import math
+subjects = ["데이터베이스", "C++", "5", "Java", "Python", "Java", "9", "리눅스"]
+#subjects = ["데이터베이스", "C++", 5, "Java", "Python", "Java", "9", "리눅스"] # not support between instance of int and str
+#print(subjects[::-1])
+#subjects[::-1]
+#subjects = subjects[::-1]  # subjects.reverse()
+print(subjects)
 
+# delete
+#subjects.remove("Java")
+#del subjects[-2]
+#del subjects[2]
+#print(subjects.pop(2)) # delete () after return ()
 
-def my_pow(b, e) -> float:
-    """
-    A user-defined function that receives a base and exponent and returns the power result in the form of a real number
-    :param b: base number
-    :param e: exponent
-    :return: the power result in the form of a real number
-    """
-    if e < 0:
-        b = 1 / b
-        e = e * -1
-
-    result = 1
-
-    i = int(e)
-    f = e - i
-
-    for _ in range(i):  # for k in range(e):
-        result = result * b
-
-    if f > 0:
-        result = result * math.exp(f * math.log(b))
-
-    return result
-
-
-print(my_pow(10, -2))
-print(my_pow(2, 9))     #512
-print(my_pow(16, 0.5))  #4
-print(my_pow(10, 3))    #1000
-print(my_pow(25, 0.5))  #5
-
-# print(math.exp(1))
-# print(math.e)
-# print(math.log(16, 2))
-
+# arrange
+subjects.sort(reverse=True)  # desc / modified origin value
+subjects.sort(reverse=False) # ascd
+copy_subjects = sorted(subjects) # not modified origin value
+print(subjects)
+#print(copy_subjects)
