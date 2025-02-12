9a9470260555252f3f9d5e43c374bdf1d8554ef5
gee989185 <gee020915@gmail.com>
2025-02-12 14:02:16 +0900

    v2.5 day03

--- day03.py
+++ day03.py
@@ -1,19 +1,11 @@
-subjects = ["데이터베이스", "C++", "5", "Java", "Python", "Java", "9", "리눅스"]
-#subjects = ["데이터베이스", "C++", 5, "Java", "Python", "Java", "9", "리눅스"] # not support between instance of int and str
-#print(subjects[::-1])
-#subjects[::-1]
-#subjects = subjects[::-1]  # subjects.reverse()
-print(subjects)
-
-# delete
-#subjects.remove("Java")
-#del subjects[-2]
-#del subjects[2]
-#print(subjects.pop(2)) # delete () after return ()
-
-# arrange
-subjects.sort(reverse=True)  # desc / modified origin value
-subjects.sort(reverse=False) # ascd
-copy_subjects = sorted(subjects) # not modified origin value
-print(subjects)
-#print(copy_subjects)
+import copy
+#subjects = ["a", "b", "c"]         # immutable
+subjects = ["a", ["b", "c"], "d"]   # include mutable(subjects[1,2])
+a = subjects                        # reference
+b = subjects.copy()                 # shallow copy
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)                # deep copy
+print(subjects, a, b, c, d, e)
+subjects[1][1] = "x"
+print(subjects, a, b, c, d, e)
