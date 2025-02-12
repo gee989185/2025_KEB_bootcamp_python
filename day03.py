4364b18e4b607bc581f0edf2fb930cc2c2ab1fa3
gee989185 <gee020915@gmail.com>
2025-02-12 14:07:00 +0900

    v2.6 day03 list comprehesion

--- day03.py
+++ day03.py
@@ -1,11 +1,22 @@
-import copy
-#subjects = ["a", "b", "c"]         # immutable
-subjects = ["a", ["b", "c"], "d"]   # include mutable(subjects[1,2])
-a = subjects                        # reference
-b = subjects.copy()                 # shallow copy
-c = list(subjects)
-d = subjects[:]
-e = copy.deepcopy(a)                # deep copy
-print(subjects, a, b, c, d, e)
-subjects[1][1] = "x"
-print(subjects, a, b, c, d, e)
+# squares = list()
+# squares.append(1*1)
+# squares.append(2*2)
+# squares.append(3*3)
+# squares.append(4*4)
+# squares.append(5*5)
+# print(squares)
+
+
+#squares = list()
+# for i in range(1, 6, 1):
+#     squares.append(i*i)
+# print(squares)
+
+
+# #list comprehension
+squares = [i*i for i in range(1, 6, 1)]
+print(squares)
+
+
+even_squares = [i*i for i in range(1, 6, 1) if i % 2 == 0]
+print(even_squares)
