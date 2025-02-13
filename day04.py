0266bb65da720dae0517fb6062368cd33a659b7f
gee989185 <gee020915@gmail.com>
2025-02-13 14:28:04 +0900

    v3.3 day04 ãDecorator

--- day04.py
+++ day04.py
@@ -3,15 +3,28 @@
 
 import time
 
-# !(factorial)
+
+def time_decorator(func):
+    def wrapper(*arg):
+        start_time = time.time()
+        result = func(*arg)
+        end_time = time.time()
+        print(f"실행시간은 {end_time - start_time}초 걸렸습니다")
+        return result
+    return wrapper
+
+@time_decorator
 def factorial_repetition(n) -> int:
     result = 1
     for i in range(2, n+1):
         result *= i
     return result
 
+
+
+
 number = int(input())
-start_time = time.time()
+#start_time = time.time()
 print(f"{number}! = {factorial_repetition(number)}")
-end_time =  time.time()
-print(end_time - start_time)
+#end_time =  time.time()
+#print(end_time - start_time)
