f94dde22f237fea81273948c1e2128c8e7ee5e99
gee989185 <gee020915@gmail.com>
2025-02-13 11:15:34 +0900

    v2.9 day04

--- day03.py
+++ day03.py
@@ -12,16 +12,39 @@ import random
 drinks_foods_sul = ["위스키", "와인","소주","고량주"]
 drinks_foods_anzu = ["초콜릿","치즈", "삼겹살", "양꼬치" ]
 prices_sul = [50_000, 30_000, 5_000, 7_500]
+amounts = [0 for i in range(len(drinks_foods_sul))]
+# amounts = list()
+# for i in range(len(drinks_foods_sul)):
+#     amounts.append(0)
 
 
-# 술, 안주 추가
-drinks_foods_sul.extend(["사케", "막걸리"])
-drinks_foods_anzu.extend(["광어회", "낙곱새"])
-prices_sul.extend([20_000, 3_000])
+
+# # 술, 안주 추가(extend)
+# drinks_foods_sul.extend(["사케", "막걸리"])
+# drinks_foods_anzu.extend(["광어회", "낙곱새"])
+# prices_sul.extend([20_000, 3_000])
 
 
-def print_menu(n):
+# 술, 안주 추가(append)
+drinks_foods_sul.append("사케")
+drinks_foods_anzu.append("광어회")
+prices_sul.append(20_000)
+prices_sul.append(3_000)
+amounts.append(0)
+drinks_foods_anzu[0] = "낙곱새"
+
+
+
+
+ total_price = 0
+
+def print_menu_total_price(n):
+    global total_price
     print(f'{drinks_foods_sul[n]}에 어울리는 안주는 {drinks_foods_anzu[n]} 입니다')
+    print(f'가격은  {prices_sul[n]} 입니다')
+    amounts[n] = amounts[n] + 1
+    total_price = total_price + prices_sul[n]
+
 
 
 
@@ -34,7 +57,7 @@ menu_list = menu_list + f'{len(drinks_foods_sul) +1}) 아무거나\t\t {len(drin
 while True:
     menu = int(input(menu_list))
     if 1 <= menu <= len(drinks_foods_anzu):
-        print_menu(menu - 1)
+        print_menu_total_price(menu - 1)
     elif menu == len(drinks_foods_anzu) +1:
         random_index = random.randint(0, len(drinks_foods_anzu) - 1)
         print(f'{drinks_foods_sul[random_index]}에 어울리는 안주는 {drinks_foods_anzu[random_index]}입니다.')
@@ -42,3 +65,15 @@ while True:
     else:
         print(f'다음에 또 오세요')
         break
+
+# for k in range(len(drinks_foods_sul)):
+#     if amounts[k] != 0:
+#         print(f"주류명: {drinks_foods_sul[k]} 수량: {amounts[k]} 단가:{prices_sul[k]} 소계: {prices_sul[k] * amounts[k]}")
+# print(f"총 금액: {total_price}원")
+
+
+print("\n=== 주문 내역 ===")
+for i in range(len(drinks_foods_sul)):
+    if amounts[i] > 0:
+        print(f'주류명: {drinks_foods_sul[i]}\n | 수량: {amounts[i]:>2} | 단가: {prices_sul[i]:>6} | 소계: {prices_sul[i] * amounts[i]:>5}')
+print(f'총 금액: {total_price}원')
